010959b5b6a4c236c2bbca1677045eaae26116bd
Raj <ssukhdevsingh0128@gmail.com>
2025-05-01 23:40:08 +0530

    added model class and resume-repo using jpa

---
+++ src/main/java/com/resumeparser/repository/ResumeRepository.java
@@ -0,0 +1,8 @@
+package com.resumeparser.repository;
+
+import com.resumeparser.model.Resume;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResumeRepository extends JpaRepository<Resume,Long> {
+
+}
