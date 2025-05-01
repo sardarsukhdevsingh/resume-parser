903164af515e3f6317f900faf8e0b17aaf80dfea
Raj <ssukhdevsingh0128@gmail.com>
2025-05-01 18:25:09 +0530

    added resume-parser project with proper structure

---
+++ src/main/java/com/resumeparser/Application.java
@@ -0,0 +1,13 @@
+package com.resumeparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
