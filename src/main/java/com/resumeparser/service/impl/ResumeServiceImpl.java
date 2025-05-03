8bdb150c43e076a98197b601dd412514d8171952
Raj <ssukhdevsingh0128@gmail.com>
2025-05-03 16:30:37 +0530

    updated pdf and docx version, added logic for extracting text

--- src/main/java/com/resumeparser/service/impl/ResumeServiceImpl.java
+++ src/main/java/com/resumeparser/service/impl/ResumeServiceImpl.java
@@ -1,10 +1,16 @@
 package com.resumeparser.service.impl;
 
 import com.resumeparser.service.ResumeService;
+//import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -37,6 +43,22 @@ public class ResumeServiceImpl implements ResumeService {
         }
         Path filepath=uploadDir.resolve(filename);
         Files.copy(file.getInputStream(),filepath, StandardCopyOption.REPLACE_EXISTING);
+
+        if(filename.toLowerCase().endsWith(".pdf")){
+            try(PDDocument document=PDDocument.load(filepath.toFile())){
+                PDFTextStripper pdfTextStripper=new PDFTextStripper();
+                String text=pdfTextStripper.getText(document);
+                System.out.println("Extracted from pdf : "+text);
+            }
+        }
+        else if (filename.toLowerCase().endsWith(".docx")){
+            try (FileInputStream fis=new FileInputStream(filepath.toFile())){
+                XWPFDocument doc=new XWPFDocument(fis);
+                XWPFWordExtractor extractor=new XWPFWordExtractor(doc);
+                String text=extractor.getText();
+                System.out.println("TEXT from docx : "+text);
+            }
+        }
 
         System.out.println("filename : "+filename);
         System.out.println("content type : "+contentType);
